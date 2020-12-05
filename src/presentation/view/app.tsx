325ff1183eb1811a16e3692a083a2a67ee6c2bb2
wako-p <o9o76gp381w@gmail.com>
2020-12-05 14:23:15 +0900

    不要なコンポーネントを削除

--- src/presentation/view/app.tsx
+++ src/presentation/view/app.tsx
@@ -1,55 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { MemoryRouter, Switch, Route } from "react-router-dom";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-
-import { NavBar } from "src/presentation/view/components/presentational/organisms/nav-bar";
-import { Home } from "src/presentation/view/components/presentational/pages/home";
-import { About } from "src/presentation/view/components/presentational/pages/about";
 
 type AppProps = {
 }
 
 export const App = (props: AppProps) => {
-
-    const [ message, setMessage ] = useState("Hello React!!");
-    const classes = useStyles();
-
-    const handleTestPromiseButtonClick = () => {
-        const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-        (async () => {
-
-            for (let i = 3; i > 0; i--) {
-                await wait(1000);
-                setMessage(`${i}`);
-            }
-
-            await wait(1000);
-            setMessage("React, completely understood!!");
-        })();
-    };
-
-    const handleTestActiveXButtonClick = () => {
-        const wsh = new ActiveXObject("WScript.Shell");
-        wsh.Run("notepad");
-    };
-
     return (
         <MemoryRouter>
-            <NavBar />
             <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
+                <Route exact path="/" />
             </Switch>
         </MemoryRouter>
     );
 
 };
-
-const useStyles = makeStyles((theme: Theme) => 
-    createStyles({
-        root: {
-            "& > *": {
-                margin: theme.spacing(1),
-            },
-        },
-}));
